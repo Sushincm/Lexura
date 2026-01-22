e70991ddbe8540e074d845ccf4380c57c0b83ae7
Sushincm <sushinofficial@gmail.com>
2026-01-22 20:21:33 +0530

    product section changed

---
+++ js/owl-custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".mobile-product-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    items: 1,
+  });
+});
